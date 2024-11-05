dc7bceee418f535a26a727a16128e4d1a59ce212
13550343990 <ilikehome@163.com>
2024-11-05 21:33:57 +0800

    dev ok

--- test_main.py
+++ test_main.py
@@ -11,29 +11,23 @@ class devClass:
         self.hikSDK, self.playM4SDK = self.LoadSDK()  # 加载sdk库
         self.iUserID = -1  # 登录句柄
         self.lRealPlayHandle = -1  # 预览句柄
-        self.wincv = None  # windows环境下的参数
-        self.win = None  # 预览窗口
         self.FuncDecCB = None  # 解码回调
         self.PlayCtrlPort = C_LONG(-1)  # 播放通道号
-        self.basePath = ''  # 基础路径
-        self.preview_file = ''  # linux预览取流保存路径
+        self.basePath = ''
+        self.preview_file = ''
         self.funcRealDataCallBack_V30 = REALDATACALLBACK(self.RealDataCallBack_V30)  # 预览回调函数
-        # self.msg_callback_func = MSGCallBack_V31(self.g_fMessageCallBack_Alarm)  # 注册回调函数实现
 
     def LoadSDK(self):
         hikSDK = None
         playM4SDK = None
         try:
-            print("netsdkdllpath: ", netsdkdllpath)
             hikSDK = load_library(netsdkdllpath)
             playM4SDK = load_library(playM4dllpath)
         except OSError as e:
             print('动态库加载失败', e)
         return hikSDK, playM4SDK
 
-    # 设置SDK初始化依赖库路径
     def SetSDKInitCfg(self):
-        # 设置HCNetSDKCom组件库和SSL库加载路径
         if sys_platform == 'windows':
             basePath = os.getcwd().encode('gbk')
             strPath = basePath + b'\\lib\\win'
@@ -65,11 +59,7 @@ class devClass:
                 print('NET_DVR_SetSDKInitCfg: 4 Succ')
         self.basePath = basePath
 
-    # 通用设置，日志/回调事件类型等
     def GeneralSetting(self):
-
-        # 日志的等级（默认为0）：0-表示关闭日志，1-表示只输出ERROR错误日志，2-输出ERROR错误信息和DEBUG调试信息，3-输出ERROR错误信息、DEBUG调试信息和INFO普通信息等所有信息
-        # self.hikSDK.NET_DVR_SetLogToFile(3, b'./SdkLog_Python/', False)
         self.hikSDK.NET_DVR_SetLogToFile(3, bytes('./SdkLog_Python/', encoding="utf-8"), False)
 
     # 登录设备
@@ -93,194 +83,73 @@ class devClass:
         else:
             print('登录成功，设备序列号：%s' % str(struDeviceInfoV40.struDeviceV30.sSerialNumber, encoding="utf8").rstrip('\x00'))
 
-    # 登出设备
-    def LogoutDev(self):
-        if self.iUserID > -1:
-            # 撤销布防，退出程序时调用
-            self.hikSDK.NET_DVR_Logout(self.iUserID)
-
-    def DecCBFun(self, nPort, pBuf, nSize, pFrameInfo, nUser, nReserved2):
-        # 解码回调函数
-        if pFrameInfo.contents.nType == 3:
-            # 解码返回视频YUV数据，将YUV数据转成jpg图片保存到本地
-            # 如果有耗时处理，需要将解码数据拷贝到回调函数外面的其他线程里面处理，避免阻塞回调导致解码丢帧
-            sFileName = ('./pic/test_stamp[%d].jpg' % pFrameInfo.contents.nStamp)
-            nWidth = pFrameInfo.contents.nWidth
-            nHeight = pFrameInfo.contents.nHeight
-            nType = pFrameInfo.contents.nType
-            dwFrameNum = pFrameInfo.contents.dwFrameNum
-            nStamp = pFrameInfo.contents.nStamp
-            print(nWidth, nHeight, nType, dwFrameNum, nStamp, sFileName)
-
-            lRet = self.playM4SDK.PlayM4_ConvertToJpegFile(pBuf, nSize, nWidth, nHeight, nType,
-                                                           c_char_p(sFileName.encode()))
-            if lRet == 0:
-                print('PlayM4_ConvertToJpegFile fail, error code is:', self.playM4SDK.PlayM4_GetLastError(nPort))
-            else:
-                print('PlayM4_ConvertToJpegFile success')
-
-    # 将视频流保存到本地
-    def writeFile(self, filePath, pBuffer, dwBufSize):
-        # 使用memmove函数将指针数据读到数组中
-        data_array = (c_byte * dwBufSize)()
-        memmove(data_array, pBuffer, dwBufSize)
-
-        # 判断文件路径是否存在
-        if not os.path.exists(filePath):
-            # 如果不存在，使用 open() 函数创建一个空文件
-            open(filePath, "w").close()
-
-        preview_file_output = open(filePath, 'ab')
-        preview_file_output.write(data_array)
-        preview_file_output.close()
 
     def RealDataCallBack_V30(self, lPlayHandle, dwDataType, pBuffer, dwBufSize, pUser):
-        # 码流回调函数
-        if sys_platform == 'xx':
-            # 码流回调函数
-            if dwDataType == NET_DVR_SYSHEAD:
-                from datetime import datetime
-                # 获取当前时间的datetime对象
-                current_time = datetime.now()
-                timestamp_str = current_time.strftime('%Y%m%d_%H%M%S')
-                self.preview_file = f'./previewVideo{timestamp_str}.mp4'
-            elif dwDataType == NET_DVR_STREAMDATA:
-                self.writeFile(self.preview_file, pBuffer, dwBufSize)
-            else:
-                print(u'其他数据,长度:', dwBufSize)
-        elif sys_platform == 'linux' or sys_platform == 'windows':
-            if dwDataType == NET_DVR_SYSHEAD:
-                # 设置流播放模式
-                self.playM4SDK.PlayM4_SetStreamOpenMode(self.PlayCtrlPort, 0)
-                # 打开码流，送入40字节系统头数据
-                if self.playM4SDK.PlayM4_OpenStream(self.PlayCtrlPort, pBuffer, dwBufSize, 1024 * 1024):
-                    # 设置解码回调，可以返回解码后YUV视频数据
-                    # self.FuncDecCB = DECCBFUNWIN(self.DecCBFun) #by shenhao
-                    # self.playM4SDK.PlayM4_SetDecCallBackExMend(self.PlayCtrlPort, self.FuncDecCB, None, 0, None) #by shenhao
-                    # 开始解码播放
-                    if self.playM4SDK.PlayM4_Play(self.PlayCtrlPort, None):
-                        print(u'播放库播放成功')
-                    else:
-                        print(u'播放库播放失败')
+        if dwDataType == NET_DVR_SYSHEAD:
+            # 设置流播放模式
+            self.playM4SDK.PlayM4_SetStreamOpenMode(self.PlayCtrlPort, 0)
+            # 打开码流，送入40字节系统头数据
+            if self.playM4SDK.PlayM4_OpenStream(self.PlayCtrlPort, pBuffer, dwBufSize, 1024 * 1024):
+                # 开始解码播放
+                if self.playM4SDK.PlayM4_Play(self.PlayCtrlPort, None):
+                    print(u'播放库播放成功')
                 else:
-                    print(f'播放库打开流失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
-            elif dwDataType == NET_DVR_STREAMDATA:
-                self.playM4SDK.PlayM4_InputData(self.PlayCtrlPort, pBuffer, dwBufSize)
-
-                p_width = ctypes.c_int(0)
-                p_height = ctypes.c_int(0)
-                if not self.playM4SDK.PlayM4_GetPictureSize(self.PlayCtrlPort, ctypes.byref(p_width),
-                                                             ctypes.byref(p_height)):
-                    print(f'获取PlayM4_GetPictureSize失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
-                print(f"PlayM4_GetPictureSize value: {p_width.value}, {p_height.value}")
-
-                pic_buff_size = p_width.value * p_height.value * 5
-                jpeg_buffer = (ctypes.c_ubyte * pic_buff_size)()
-                real_pic_size = ctypes.c_int(0)
-                result = self.playM4SDK.PlayM4_GetJPEG(self.PlayCtrlPort, jpeg_buffer, pic_buff_size,
-                                                        ctypes.byref(real_pic_size))
-                if result == 1:
-                    print(f"PlayM4_GetJPEG value: {real_pic_size.value}")
-
-                    # 保存为 JPEG 文件
-                    img_filename = f'pic/image_{time.time()}.jpg'
-                    with open(img_filename, 'wb') as f:
-                        f.write(bytes(jpeg_buffer[:real_pic_size.value]))
-
-                else:
-                    print(f'抓图PlayM4_GetJPEG失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
+                    print(u'播放库播放失败')
             else:
-                print(u'其他数据,长度:', dwBufSize)
+                print(f'播放库打开流失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
+        elif dwDataType == NET_DVR_STREAMDATA:
+            self.playM4SDK.PlayM4_InputData(self.PlayCtrlPort, pBuffer, dwBufSize)
 
-    def startPlay(self, playTime):
+            p_width = ctypes.c_int(0)
+            p_height = ctypes.c_int(0)
+            if not self.playM4SDK.PlayM4_GetPictureSize(self.PlayCtrlPort, ctypes.byref(p_width),
+                                                        ctypes.byref(p_height)):
+                print(f'获取PlayM4_GetPictureSize失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
+            print(f"PlayM4_GetPictureSize value: {p_width.value}, {p_height.value}")
+
+            pic_buff_size = p_width.value * p_height.value * 5
+            jpeg_buffer = (ctypes.c_ubyte * pic_buff_size)()
+            real_pic_size = ctypes.c_int(0)
+            result = self.playM4SDK.PlayM4_GetJPEG(self.PlayCtrlPort, jpeg_buffer, pic_buff_size,
+                                                   ctypes.byref(real_pic_size))
+            if result == 1:
+                print(f"PlayM4_GetJPEG value: {real_pic_size.value}")
+
+                # 保存为 JPEG 文件
+                img_filename = f'pic/image_{time.time()}.jpg'
+                with open(img_filename, 'wb') as f:
+                    f.write(bytes(jpeg_buffer[:real_pic_size.value]))
+
+            else:
+                print(f'抓图PlayM4_GetJPEG失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
+        else:
+            print(u'其他数据,长度:', dwBufSize)
+
+
+    def start_play(self):
         # 获取一个播放句柄
         if not self.playM4SDK.PlayM4_GetPort(byref(self.PlayCtrlPort)):
             print(f'获取播放库句柄失败, 错误码：{self.playM4SDK.PlayM4_GetLastError(self.PlayCtrlPort)}')
 
-        if sys_platform == 'windows' or sys_platform == 'linux':
-            # 开始预览
-            preview_info = NET_DVR_PREVIEWINFO()
-            preview_info.hPlayWnd = 0
-            preview_info.lChannel = 1  # 通道号
-            preview_info.dwStreamType = 0  # 主码流
-            preview_info.dwLinkMode = 0  # TCP
-            preview_info.bBlocked = 1  # 阻塞取流
+        # 开始预览
+        preview_info = NET_DVR_PREVIEWINFO()
+        preview_info.hPlayWnd = 0
+        preview_info.lChannel = 1  # 通道号
+        preview_info.dwStreamType = 0  # 主码流
+        preview_info.dwLinkMode = 0  # TCP
+        preview_info.bBlocked = 1  # 阻塞取流
 
-            # 开始预览并且设置回调函数回调获取实时流数据
-            self.lRealPlayHandle = self.hikSDK.NET_DVR_RealPlay_V40(self.iUserID, byref(preview_info),
-                                                                    self.funcRealDataCallBack_V30,
-                                                                    None)
-            if self.lRealPlayHandle < 0:
-                print('Open preview fail, error code is: %d' % self.hikSDK.NET_DVR_GetLastError())
-                # 登出设备
-                self.hikSDK.NET_DVR_Logout(self.iUserID)
-                # 释放资源
-                self.hikSDK.NET_DVR_Cleanup()
-                exit()
-            time.sleep(playTime)
-
-        elif sys_platform == 'xx':
-            import tkinter
-            from tkinter import Button
-
-            # 创建窗口
-            self.win = tkinter.Tk()
-            # 固定窗口大小
-            self.win.resizable(0, 0)
-            self.win.overrideredirect(True)
-
-            sw = self.win.winfo_screenwidth()
-            # 得到屏幕宽度
-            sh = self.win.winfo_screenheight()
-            # 得到屏幕高度
-
-            # 窗口宽高
-            ww = 512
-            wh = 384
-            x = (sw - ww) / 2
-            y = (sh - wh) / 2
-            self.win.geometry("%dx%d+%d+%d" % (ww, wh, x, y))
-
-            # 创建退出按键
-            b = Button(self.win, text='退出', command=self.win.quit)
-            b.pack()
-            # 创建一个Canvas，设置其背景色为白色
-            self.wincv = tkinter.Canvas(self.win, bg='white', width=ww, height=wh)
-            self.wincv.pack()
-
-            # 开始预览
-            preview_info = NET_DVR_PREVIEWINFO()
-            preview_info.hPlayWnd = 0
-            preview_info.lChannel = 1  # 通道号
-            preview_info.dwStreamType = 0  # 主码流
-            preview_info.dwLinkMode = 0  # TCP
-            preview_info.bBlocked = 1  # 阻塞取流
-
-            # 开始预览并且设置回调函数回调获取实时流数据
-            self.lRealPlayHandle = self.hikSDK.NET_DVR_RealPlay_V40(self.iUserID, byref(preview_info),
-                                                                    self.funcRealDataCallBack_V30,
-                                                                    None)
-            if self.lRealPlayHandle < 0:
-                print('Open preview fail, error code is: %d' % self.hikSDK.NET_DVR_GetLastError())
-                # 登出设备
-                self.hikSDK.NET_DVR_Logout(self.iUserID)
-                # 释放资源
-                self.hikSDK.NET_DVR_Cleanup()
-                exit()
-
-            # show Windows
-            self.win.mainloop()
-
-    def stopPlay(self):
-        # 关闭预览
-        self.hikSDK.NET_DVR_StopRealPlay(self.lRealPlayHandle)
-
-        # 停止解码，释放播放库资源
-        if self.PlayCtrlPort.value > -1:
-            self.playM4SDK.PlayM4_Stop(self.PlayCtrlPort)
-            self.playM4SDK.PlayM4_CloseStream(self.PlayCtrlPort)
-            self.playM4SDK.PlayM4_FreePort(self.PlayCtrlPort)
-            self.PlayCtrlPort = C_LONG(-1)
+        # 开始预览并且设置回调函数回调获取实时流数据
+        self.lRealPlayHandle = self.hikSDK.NET_DVR_RealPlay_V40(self.iUserID, byref(preview_info),
+                                                                self.funcRealDataCallBack_V30,
+                                                                None)
+        if self.lRealPlayHandle < 0:
+            print('Open preview fail, error code is: %d' % self.hikSDK.NET_DVR_GetLastError())
+            # 登出设备
+            self.hikSDK.NET_DVR_Logout(self.iUserID)
+            # 释放资源
+            self.hikSDK.NET_DVR_Cleanup()
+            exit()
 
 
 if __name__ == '__main__':
@@ -290,10 +159,5 @@ if __name__ == '__main__':
     dev.GeneralSetting()  # 通用设置，日志，回调函数等
     dev.LoginDev(ip=b'169.254.43.56', username=b"admin", pwd=b"tongda2024")  # 登录设备
 
-    dev.startPlay(playTime=5)  # playTime用于linux环境控制预览时长，windows环境无效
-    time.sleep(10)
-
-    dev.stopPlay()
-    dev.LogoutDev()
-    # 释放资源
-    dev.hikSDK.NET_DVR_Cleanup()
+    dev.start_play()  # playTime用于linux环境控制预览时长，windows环境无效
+    time.sleep(20)
